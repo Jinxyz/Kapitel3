cf72ffc4b0c5a59eae3a6a4b6e0e479112860ef4
Rasmus Eriksson Gillberg <rasmus.gillberg@learnet.se>
2019-09-26 11:18:59 +0200

    Torsdag 26/9

---
+++ Upp31.java
@@ -0,0 +1,22 @@
+/*
+ Uppgift3.1
+ Skapat 2019-09-26
+ Rasmus Gillberg
+*/
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Upp31 {
+	public static void main(String[] args) {
+		
+		int i;
+		int d;
+		int b;
+		i = 500;
+		d = 500;
+		b = 500;
+		
+		
+	}
+}
---
+++ Upp32.java
@@ -0,0 +1,23 @@
+/*
+ Uppgift3.2
+ Skapat 2019-09-26
+ Rasmus Gillberg
+*/
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Upp32 {
+	public static void main(String[] args) {
+		
+		int i;
+		int d;
+		int b;
+		// Funkar ej på grund av att int bara läser heltal
+		i = 500.7;
+		d = 500.7;
+		b = 500.7;
+		
+		
+	}
+}
